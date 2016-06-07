77aa489eff16f949706759d4bb2569e95ae4acd4
ojokbernard <ojokbernard@googlemail.com>
2016-06-07 12:35:40 +0300

    Update Contact us.php

--- Contact us.php
+++ Contact us.php
@@ -36,37 +36,26 @@
 		
 		<div class="container">
 			<h1>Gulu Memoirs Marathon Official site</h1>
-			<h2>Registrater</h2>
+			<h2>Registration</h2>
 			<h3>To participate sign up here</h3>
 			
-			<form action="handle_registration.php" method="get">
-				<div class="row">
-					<div class="col-sm-2">
-						<label for="inputEmail3" class="form-control-label form-control-static">Email</label>
-					</div>
-					<div class="col-sm-6">
-						<input type="email" class="form-control" name="email" id="inputEmail3" placeholder="Email">
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-sm-2">
-						<label class="form-control-label form-control-static" for="name">Full Name</label>
-					</div>
-					<div class="col-sm-6">
-						<input type="text" class="form-control" id="name" name="name" placeholder="enter your name"/>
-					</div>
-				</div>
-				 <div class="row">
-					<div class="col-sm-2">
-						<label class="form-control-label form-control-static" for="telephone_number">Telephone</label>
-					</div>
-					<div class="col-sm-6">
-						<input type="text" class="form-control" id="telephone_number" name="telephone_number" placeholder="enter your telephone"/>
-					</div>
-				</div>
-				<input class="btn btn-success" type="submit" value="Register"/>
 			
-			</form>
+				<form action="mail.php" method="POST">
+<p>Name</p> <input type="text" name="name">
+<p>Email</p> <input type="text" name="email">
+<p>Message</p><textarea name="message" rows="6" cols="25"></textarea><br />
+<input type="submit" value="Send"><input type="reset" value="Clear">
+</form>
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "ojokbernard@googlemail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
 		</div>
 		
 		<script src="js/jquery.min.js"></script>
